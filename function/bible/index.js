14ed737dfc83f94996d4e831defbf0f0ce07ec2c
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2019-12-31 15:37:55 -0300

    Funçao para retornar quais idiomas sao validos

--- function/bible/index.js
+++ function/bible/index.js
@@ -1,6 +1,16 @@
+const fs = require('fs')
+
 const getBible = ( language, version ) => {
     const bible = require(`./${language}/${version}`)()
     return bible
 }
 
+const getValidLanguageOptions = () =>{
+    const language = fs.readdirSync(__dirname)
+    const removeIndexJs = language.indexOf('index.js')
+    language.splice(removeIndexJs, 1)
+    return language
+}
+
 console.log(getBible("pt-br", "NVI").judas[1][1])
+console.log(getValidLanguageOptions())
