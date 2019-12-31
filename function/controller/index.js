a977fc78d642c957537fc685e637516321948b6f
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2019-12-31 18:17:31 -0300

    Criaçao de uma funçao para juntar os objetos caso alguma nova configuraçao seja passada e respotando os dados da configuraçao para consulta

--- function/controller/index.js
+++ function/controller/index.js
@@ -1,20 +1,19 @@
 const bible = require( '../bible' )
 
-const data = {
-    language: 'pt-br',
-    version: 'NVI'
-}
+const data = {}
+
 
 const testIfBookIsValid = () => {
-
+    
 }
 
-module.exports.full = ( objectData = data ) => bible( objectData.language, objectData.version )
 
+module.exports.dataConfig = data
+module.exports.joinCallData = ( callData ) => Object.assign( data, callData)
+module.exports.full = ( objectData = data ) => bible( objectData.language, objectData.version )
 module.exports.titles = ( objectData = data ) => {
     const fullBible = bible( objectData.language, objectData.version )
     const bibleBooks = Object.keys(fullBible.books)
     return bibleBooks.map( bookName => fullBible.books[bookName].data.title )
 } 
-
 module.exports.data = ( objectData = data ) => bible( objectData.language, objectData.version ).data
--- function/test.js
+++ function/test.js
@@ -1,9 +1,12 @@
 const bible = require("./controller")
 const data = {
-    language: '',
+    language: 'en',
     version: 'NVI'
 }
 
 console.log(bible.full( data ).books.genesis.data);
 console.log(bible.titles( data ))
 console.log(bible.data( data ));
+console.log(bible.dataConfig)
+bible.joinCallData( data )
+console.log(bible.dataConfig)
