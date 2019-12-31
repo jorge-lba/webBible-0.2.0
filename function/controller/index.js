0ae6fa40c8383adf2998c0aeb15d3d838014fd87
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2019-12-31 17:41:46 -0300

    Criando funçoes que retornam a biblia completa, dados e titulo dos livros / Feito alguns testes basicos

--- function/controller/index.js
+++ function/controller/index.js
@@ -1,3 +1,11 @@
 const bible = require( '../bible' )
 
-module.exports = bible
+module.exports.fullBible = bible
+
+module.exports.booksNames = ( language = 'pt-br', version = 'NVI' ) => {
+    const fullBible = bible( language, version )
+    const bibleBooks = Object.keys(fullBible.books)
+    return bibleBooks.map( bookName => fullBible.books[bookName].data.title )
+} 
+
+module.exports.bibleData = ( language = 'pt-br', version = 'NVI' ) => bible( language, version ).data
--- function/test.js
+++ function/test.js
@@ -1,3 +1,5 @@
 const controller = require("./controller")
 
-console.log(controller);
+console.log(controller.fullBible('en').books.genesis.data);
+console.log(controller.booksNames())
+console.log(controller.bibleData());
