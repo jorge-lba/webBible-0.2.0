eae0eb78b0619f300a065d8d4e3998c7fed558a3
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2019-12-31 16:01:43 -0300

    Funçao que retorna as versoes validas e funçao para vervidar se a chama e valida

--- function/bible/index.js
+++ function/bible/index.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 
-const getBible = ( language, version ) => {
-    const bible = require(`./${language}/${version}`)()
-    return bible
-}
+const getBible = ( language, version ) => require( `./${language}/${version}` )()
 
-const getValidLanguageOptions = () =>{
-    const language = fs.readdirSync(__dirname)
-    const removeIndexJs = language.indexOf('index.js')
-    language.splice(removeIndexJs, 1)
+const getValidLanguageOptions = () => {
+    const language = fs.readdirSync (__dirname )
+    const removeIndexJs = language.indexOf( 'index.js' )
+    language.splice( removeIndexJs, 1 )
     return language
 }
+const getValidVersionOptions = ( language ) => fs.readdirSync( __dirname+'/'+language )    
 
-console.log(getBible("pt-br", "NVI").judas[1][1])
-console.log(getValidLanguageOptions())
+const testIfCallIsValid = ( call, options ) => options.indexOf(call) < 0 ? false : true
+
+
+console.log( getBible( "pt-br", "NVI" ).judas[1][1] )
+console.log( getValidLanguageOptions() )
+console.log( getValidVersionOptions( getValidLanguageOptions()[9]) )
+console.log( testIfCallIsValid( 'NVI', getValidVersionOptions( getValidLanguageOptions()[9] ) ) )
