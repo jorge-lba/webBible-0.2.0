28ae6e50bd76e0e652738b9b77ffab1f38ed622b
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-13 23:42:56 -0300

    Testando requisiçoes

--- functions/src/test.js
+++ functions/src/test.js
@@ -1,4 +1,5 @@
 const bible = require("./index")
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 const dataDefault = {
     dataDefault: {
@@ -18,8 +19,32 @@ const dataDefault = {
             }
         }
     },
-    call: ['random-verse']
+    call: ['random-verse', 'random-verse', 'verse']}
+
+const data = JSON.stringify( dataDefault )
+var ajax = new XMLHttpRequest();
+
+// Seta tipo de requisição: Post e a URL da API
+ajax.open("POST", "https://us-central1-webbible-kll.cloudfunctions.net/api/bible");
+ajax.setRequestHeader("Content-Type", "application/json");
+
+// Seta paramêtros da requisição e envia a requisição
+ajax.send(data);
+
+// Cria um evento para receber o retorno.
+ajax.onreadystatechange = function() {
+  // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+	if (ajax.readyState == 4 && ajax.status == 200) {
+    
+		const data = JSON.parse(ajax.responseText)
+		
+    // Retorno do Ajax
+        console.log(data.results[0])
+        console.log(data.results[1])
+        console.log(data.results[2].content)
+	}
 }
+
 
 // Opções disponiveis para call: [
 //     'languages',-----------------| Retorna os idiomas e versoes disponiveis
@@ -34,4 +59,4 @@ const dataDefault = {
 
 // Os reultado retona um objeto dentro de um array na mesma sequencias que foi feita a chamada no dataDefault.call
 
-console.log( bible(dataDefault))
+// console.log( bible(dataDefault))
