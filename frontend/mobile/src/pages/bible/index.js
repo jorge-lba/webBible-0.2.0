c34144c9eba8a9fe203c93d54eade0ac7903509c
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-20 20:53:34 -0300

    Criando componenstes para as paginas

---
+++ frontend/mobile/src/pages/bible/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Bible(){
+    return( 
+        <View>Biblia</View>
+    )
+}
+
+export default Bible
---
+++ frontend/mobile/src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Home() {
+    return(
+        <View> Home Page ! </View>
+    )
+}
+
+export default Home
