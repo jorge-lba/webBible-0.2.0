84197a4b0266bd667cf4be8071e6210d36880205
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-20 23:36:39 -0300

    Iniciando testes com modal dropdown

--- frontend/mobile/src/pages/bible/index.js
+++ frontend/mobile/src/pages/bible/index.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import ModalDropdown from 'react-native-modal-dropdown'
 
 function Bible(){
     return( 
         <View style={ styles.searchBible } >
-            
+            <ModalDropdown 
+                options={optionsBook()} 
+                defaultValue={ 'genesis' }
+                style={ styles.dropdown.button } 
+            />
         </View>
     )
 }
 
+function optionsBook(){
+    return [ 'Genesis', 'Exodos','Genesis', 'Exodos','Genesis', 'Exodos','Genesis', 'Exodos' ]
+}
+
 const styles = {
     searchBible: {
-        flex: 1
+        
     },
     dropdown: {
-        width: 100,
-        heigth: 10,
-        backgroundColor: '#FF0'
+        button: {
+            width: 100,
+            height: 25,
+            backgroundColor: '#F0f800'
+        }
     }
 }
 
