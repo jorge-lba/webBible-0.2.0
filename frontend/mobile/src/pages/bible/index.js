f0c474f65c028b560de5d1f672ec5e673921a37d
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-20 22:02:39 -0300

    Add dependencia react-native-modal-dropdown

--- frontend/mobile/src/pages/bible/index.js
+++ frontend/mobile/src/pages/bible/index.js
@@ -3,10 +3,21 @@ import { View, Text } from 'react-native'
 
 function Bible(){
     return( 
-        <View>
-            <Text>Biblia</Text>
+        <View style={ styles.searchBible } >
+            
         </View>
     )
+}
+
+const styles = {
+    searchBible: {
+        flex: 1
+    },
+    dropdown: {
+        width: 100,
+        heigth: 10,
+        backgroundColor: '#FF0'
+    }
 }
 
 export default Bible
