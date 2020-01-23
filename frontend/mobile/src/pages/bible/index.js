d354c193d1745dab120d524d540c716dbea0ec0b
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-23 18:20:38 -0300

    Add lista onde sera carregado os versiculos

--- frontend/mobile/src/pages/bible/index.js
+++ frontend/mobile/src/pages/bible/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-<<<<<<< HEAD
-// import Dropdown from '../../utils/Dropdown'
+import { StyleSheet, View, Text, FlatList } from 'react-native'
 import { Dropdown } from 'react-native-material-dropdown';  // https://www.npmjs.com/package/react-native-material-dropdown
-=======
-
-import Dropdown from '../../utils/dropdown/Dropdown'
->>>>>>> 71489f8f217ea123b265d4303807e63fe0bc75c9
 
 function Bible(){
     let data = [{
@@ -41,30 +35,48 @@ function Bible(){
         value: 'Pear',
       }];
 
+    const data2 = [ 1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                    'ihdasoihdoahdoasiohdoiosnopiecne iojdoi pofjneoif fn pojfwmfd pof enfpeofjpewojf poif fokj0ewpoj fepoewjf pewjf pofj epof epofj epwokv e-woj ewpfoiew fmpeojf epof epwofuj epofuie p9ofj epfoke ik0epw9oijf e peojfekjwefpoejf w',
+                    22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40 ].map( item => {
+      return{
+        id: `${item}`,
+        title:item,
+        value: item
+      }
+    } )
+
+    function Item({ title }) {
+      return (
+        <View style={{ paddingHorizontal: 8, paddingVertical: 4, justifyContent: 'center', backgroundColor: '#FAFAFA', borderBottomColor: '#CCC', borderBottomWidth: 1 }}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      );
+    }
+
     return( 
     <>
         <View style={ styles.searchBible } >
-<<<<<<< HEAD
             <Dropdown
-            itemCount = { 8 }
-            dropdownPosition={ 0 }
-            dropdownOffset={ { top: 18, left: 0 } }
-            containerStyle={ { width: 120, position: 'absolute', left: 20 } }
-            data={data}
-        />
-=======
-            <Dropdown 
-                dButtonStyle={  styles.dropdownButton  }
-                buttonStyleText={ styles.dropdownTextStyle }
-                valueButton={ 'Genesis' }    
+                itemCount = { 8 }
+                dropdownPosition={ 0 }
+                dropdownOffset={ { top: 18, left: 0 } }
+                containerStyle={ { width: 120, justifyContent: 'center', marginHorizontal: 16} }
+                data={data}
             />
-            <Dropdown 
-                dButtonStyle={  styles.dropdownButton2  }
-                buttonStyleText={ styles.dropdownTextStyle }
-                valueButton={ '001' }    
+            <Dropdown
+                itemCount = { 8 }
+                dropdownPosition={ 0 }
+                dropdownOffset={ { top: 18, left: 0 } }
+                containerStyle={ { width: 70, justifyContent: 'center', marginHorizontal: 16} }
+                data={data2}
             />
->>>>>>> 71489f8f217ea123b265d4303807e63fe0bc75c9
         </View>
+        <FlatList
+          data={data2}
+          renderItem={({ item }) => <Item title={item.title} />}
+          keyExtractor={item => item.id}
+        />
+        <View style={ { height: 50, elevation: 3, backgroundColor: '#FFF' } } ></View>
     </>
     )
 }
@@ -77,17 +89,14 @@ function optionsBook(){
 const styles = StyleSheet.create({
     searchBible: {
         flexDirection: 'row',
-<<<<<<< HEAD
         height: 60,
         backgroundColor: '#FFF',
         elevation: 2,
+        justifyContent: 'center',
     },
     dropdownButton: {
         flex: 1,
         width: 150,
-=======
-        elevation: 1.5,
->>>>>>> 71489f8f217ea123b265d4303807e63fe0bc75c9
         height: 56,
         backgroundColor: '#888888',
         elevation: 5,
